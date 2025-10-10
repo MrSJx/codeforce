829a2fb96d3ea08a774674e6c7a8dd0bb3ddd58d
MrSJx <swapniljadhav20vs@gmail.com>
2025-10-10 21:00:08 +0530

    codeforces done for todays

---
+++ 1057(Div.2)(A.Circle_of_Apple_Trees).cpp
@@ -0,0 +1,20 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        set<int> ne(arr.begin(), arr.end());
+        cout << ne.size() << "\n";
+    }
+}
